8f153a499ef6337271548d69ee58dd48e10ad584
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-06-11 22:29:24 +0100

    text-fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="flex lg:flex-row flex-col lg:justify-evenly justify-center items-center mt-8 md:mt-20 md:px-12 px-8 py-6">
-      <h1 className="px-4 lg:text-[6rem] text-[3.2rem] text-center  lg:w-[340px] font-serif mb-[5rem] lg:mb-0">Timeless Beauty,</h1>
+      <h1 className="px-4 lg:text-[6rem] text-[3rem] text-center  lg:w-[340px] font-serif mb-[5rem] lg:mb-0">Timeless Beauty,</h1>
       <div className="relative flex self-center mx-auto h-[340px] w-full lg:w-[620px] rounded-xl bg-arrow bg-no-repeat bg-contain">
         <div className="absolute lg:left-[22%] self-center z-index-2 h-[450px] w-full lg:w-[350px] rounded-full border border-black border-thin mx-auto">
           <Image
@@ -15,7 +15,7 @@ const Hero = () => {
           />{" "}
         </div>
       </div>
-      <h1 className="lg:text-[6rem] text-[3.2rem] text-center lg:w-[340px] font-serif mt-[5rem] lg:mt-0">Infinite Steeze.</h1>
+      <h1 className="lg:text-[6rem] text-[3rem] text-center lg:w-[340px] font-serif mt-[5rem] lg:mt-0">Infinite Steeze.</h1>
     </div>
   );
 };
