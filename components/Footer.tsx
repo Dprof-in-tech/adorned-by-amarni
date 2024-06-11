004dc7b49bdb73649cab8571f4b9a18bbad9b6ff
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-06-11 22:27:28 +0100

    added links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,10 +9,19 @@ const Footer = () => {
                     <Image src="/assets/logo.svg" alt="board" width={100} height={100}/>
                 </div>
                 <div className="flex flex-row gap-4 items-center">
+                    <a href="http://wa.me/message/PGD7WM6OERBWD1**%5D(https://l.instagram.com/?u=https%3A%2F%2Fwa.me%2Fmessage%2FPGD7WM6OERBWD1&e=AT0DQcP6vlWqFSCaYLBn5Ninr7vYfuh-d8Z2excSsAvkAJIaBa-f_tSorbSGi1M7StMxiIhdvRvinopbYsrdFNoX3ThWg9rynqWtB2U_9FKQVUF3gErVxg)">
                     <Image src="/assets/call.svg" alt="board" width={40} height={40} className="w-[30px] h-[30px]"/>
+                    </a>
+                    <a href="https://twitter.com/AdornedByAmani">
                     <Image src="/assets/twitter.svg" alt="board" width={35} height={35} className="w-[30px] h-[30px]"/>
+                    </a>
+                    <a href="http://wa.me/message/PGD7WM6OERBWD1**%5D(https://l.instagram.com/?u=https%3A%2F%2Fwa.me%2Fmessage%2FPGD7WM6OERBWD1&e=AT0DQcP6vlWqFSCaYLBn5Ninr7vYfuh-d8Z2excSsAvkAJIaBa-f_tSorbSGi1M7StMxiIhdvRvinopbYsrdFNoX3ThWg9rynqWtB2U_9FKQVUF3gErVxg)">
                     <Image src="/assets/chat.svg" alt="board" width={35} height={35} className="w-[25px] h-[25px]"/>
+                    </a>
+                    <a href="https://www.instagram.com/adornedbyamani/">
                     <Image src="/assets/instagram.svg" alt="board" width={35} height={35} className="w-[25px] h-[25px]"/>
+                    </a>
+
                 </div>
             </div>
             <div className="w-full lg:w-[75%] mx-auto mt-4  p-0 md:p-4 flex flex-col lg:flex-row items-center justify-center gap-2 md:gap-6">
